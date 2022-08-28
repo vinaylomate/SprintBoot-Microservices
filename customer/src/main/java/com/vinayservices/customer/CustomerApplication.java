497db1bd7ee440741a6e7e4c12da5bea413b225d
Vinay Lomate <vinaydlomate@gamil.com>
2022-08-28 22:38:12 +0530

    Created Instances of microservices through eureka server and used load balancing for instances

--- customer/src/main/java/com/vinayservices/customer/CustomerApplication.java
+++ customer/src/main/java/com/vinayservices/customer/CustomerApplication.java
@@ -2,9 +2,11 @@ package com.vinayservices.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CustomerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CustomerApplication.class, args);
--- customer/src/main/java/com/vinayservices/customer/CustomerService.java
+++ customer/src/main/java/com/vinayservices/customer/CustomerService.java
@@ -19,7 +19,7 @@ public class CustomerService {
                 .build();
         customerRepository.saveAndFlush(customer);
         FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
-                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                "http://FRAUD/api/v1/fraud-check/{customerId}",
                 FraudCheckResponse.class,
                 customer.getId());
         if(fraudCheckResponse.isFraudster()) {
