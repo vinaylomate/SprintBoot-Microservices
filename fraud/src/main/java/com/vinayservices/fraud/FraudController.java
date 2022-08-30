1b33d56a7af63674aca4774bf26a384f1d1b16ba
Vinay Lomate <vinaydlomate@gamil.com>
2022-08-30 15:27:47 +0530

    open feign

--- fraud/src/main/java/com/vinayservices/fraud/FraudController.java
+++ fraud/src/main/java/com/vinayservices/fraud/FraudController.java
@@ -1,5 +1,6 @@
 package com.vinayservices.fraud;
 
+import com.vinaycode.clients.fraud.FraudCheckResponse;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
